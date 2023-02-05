202fdbafa4603ff06d8fc1d6dfa4bdf752dd0483
Lily Lu <lilyzhaoyilu@gmail.com>
2023-02-04 20:48:17 -0800

    Revert "Naive implementation of cache skeleton"
    This reverts commit 55bf7f77a6fd2ef6a87a84ed002be6b2da09357a.

--- develope-readme.md
+++
@@ -1,6 +0,0 @@
-
-ctrl shift b to build
-
-use launch program to run, or node ./dist/test/init-client.js
-
-npm install will show an error for renaming, it does not affect the existing program.
--- src/test/init-client.ts
+++ src/test/init-client.ts
@@ -1,8 +1,6 @@
 import { BaseClient } from '../client/index.js';
 import * as dotenv from 'dotenv';
 import { GuildMessage } from '../models/index.js';
-import { CacheManager } from '../client/event-receiver/cache.js';
-import { default as axios, AxiosInstance, AxiosResponse } from 'axios';
 
 dotenv.config();
 
@@ -14,21 +12,10 @@ const client = new BaseClient({
   logConfig: { level: 'debug' },
 });
 
-client.on('message.*', async (data: GuildMessage) => {
+client.on('message.*', (data: GuildMessage) => {
   if (data.user.bot) return;
+  console.log(client.Api.message.create(9, data.channel.id, data.content));
   console.log(data);
-  const chat_message = data.content as string;
-
-  // cache test
-  if (chat_message.includes('corgi')) {
-    const cache_res = await CacheManager.use_cache({ key: 'corgi', expiration_in_ms: 10000, excute: async () => { return await axios.get('http://worldtimeapi.org/api/timezone/America/Los_Angeles') } })
-
-    const res = cache_res.content as any;
-    const time = `Current time is: ${res.data?.datetime} ${res.data?.abbreviation}`;
-    await client.Api.message.create(9, data.channel.id, `${time}`);
-  } else {
-    client.Api.message.create(9, data.channel.id, data.content)
-  }
 });
 
 client.connect();
