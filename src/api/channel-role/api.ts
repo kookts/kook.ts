95c61ae70abd284595fef61775467b53cdc0b03d
Lily Lu <lilyzhaoyilu@gmail.com>
2023-07-04 21:07:32 -0700

    Revert "Update channel-role APIs"
    This reverts commit 1e37fa38a3b1e836153a87bb153fadc71cab56c5.

--- src/api/channel-role/api.ts
+++ src/api/channel-role/api.ts
@@ -1,25 +1,29 @@
-import { ApiBase } from '../base.js';
+import { BaseClient } from '../../client/base.js';
 import RequestError from '../../models/error/RequestError.js';
+import { GuildUser } from '../../models/user/guild.js';
 import { KAPIResponse } from '../types.js';
 import {
   KChannelRoleListResponse,
   KChannelRoleUpdateResponse,
   ChannelRoleListResponse,
   ChannelRoleUpdateResponse,
-  ChannelRoleSyncResponse,
-  KChannelRoleSyncResponse,
 } from './types.js';
 
-export class ChannelRoleAPI extends ApiBase {
+export class ChannelRoleAPI {
+  private client: BaseClient;
+  constructor(self: BaseClient) {
+    this.client = self;
+  }
+
   /**
    * 频道角色权限详情
    * @param channelId 频道id
    */
   async index(channelId: string): Promise<ChannelRoleListResponse> {
     const data = (
-      await this.client.get('v3/channel-role/index', this.toParams({
-        channelId,
-      }))
+      await this.client.get('v3/channel-role/index', {
+        channel_id: channelId,
+      })
     ).data as KAPIResponse<KChannelRoleListResponse>;
     if (data.code === 0) {
       return data.data;
@@ -40,11 +44,11 @@ export class ChannelRoleAPI extends ApiBase {
     value: string
   ): Promise<boolean> {
     const data = (
-      await this.client.post('v3/channel-role/create', this.toParams({
-        channelId,
+      await this.client.post('v3/channel-role/create', {
+        channel_id: channelId,
         type,
         value,
-      }))
+      })
     ).data as KAPIResponse<Record<string, never>>;
     if (data.code === 0) {
       return true;
@@ -53,50 +57,25 @@ export class ChannelRoleAPI extends ApiBase {
     }
   }
 
-  /**
-  * 更新频道角色权限
-  * @param channelId 频道id, 如果频道是分组的id,会同步给所有sync=1的子频道
-  * @param type value的类型, 只能为"role_id", "user_id",不传则默认为"role_id"
-  * @param value 根据 type 的值, 为用户 id 或角色 id
-  * @param allow 默认为 0, 想要设置的允许的权限值
-  * @param deny 默认为 0, 想要设置的拒绝的权限值
-  */
   async update(
     channelId: string,
     type: 'role_id' | 'user_id',
     value: string,
-    allow?: number,
+    allow: number | undefined,
     deny?: number
   ): Promise<ChannelRoleUpdateResponse> {
     if (typeof allow === 'undefined' && typeof deny === 'undefined') {
       throw new Error('Argument Required: provide either allow or deny.');
     }
     const data = (
-      await this.client.post('v3/channel-role/update', this.toParams({
-        channelId,
+      await this.client.post('v3/channel-role/update', {
+        channel_id: channelId,
         type,
         value,
         allow,
         deny,
-      }))
+      })
     ).data as KAPIResponse<KChannelRoleUpdateResponse>;
-    if (data.code === 0) {
-      return data.data;
-    } else {
-      throw new RequestError(data.code, data.message);
-    }
-  }
-
-  /**
-  * 同步频道角色权限
-  * @param channelId 频道id
-  */
-  async sync(channelId: string,): Promise<ChannelRoleSyncResponse> {
-    const data = (
-      await this.client.post('v3/channel-role/sync', this.toParams({
-        channelId
-      }))
-    ).data as KAPIResponse<KChannelRoleSyncResponse>;
     if (data.code === 0) {
       return data.data;
     } else {
@@ -112,11 +91,11 @@ export class ChannelRoleAPI extends ApiBase {
    */
   async delete(channelId: string, type: string, value = '0'): Promise<boolean> {
     const data = (
-      await this.client.post('v3/channel-role/delete', this.toParams({
-        channelId,
+      await this.client.post('v3/channel-role/delete', {
+        channel_id: channelId,
         type,
         value,
-      }))
+      })
     ).data as KAPIResponse<Record<string, never>>;
     if (data.code === 0) {
       return true;
--- src/api/channel-role/types.ts
+++ src/api/channel-role/types.ts
@@ -1,39 +1,36 @@
 import { KGuildUserData } from '../../models/index.js';
 
-interface permissionUsers {
-  user: KGuildUserData;
-  allow: number;
-  deny: number;
-}[];
-
-interface permissionOverwrites {
-  roleId: number;
-  allow: number;
-  deny: number;
-}[];
 export interface KChannelRoleListResponse {
-  permissionOverwrites: permissionOverwrites
-  permissionUsers: permissionUsers,
+  permissionOverwrites: {
+    roleId: number;
+    allow: number;
+    deny: number;
+  }[];
+  permissionUsers: {
+    user: any;
+    allow: number;
+    deny: number;
+  }[];
   permissionSync: boolean;
 }
 
 export interface ChannelRoleListResponse {
-  permissionOverwrites: permissionOverwrites
-  permissionUsers: permissionUsers,
+  permissionOverwrites: {
+    roleId: number;
+    allow: number;
+    deny: number;
+  }[];
+  permissionUsers: {
+    user: KGuildUserData;
+    allow: number;
+    deny: number;
+  }[];
   permissionSync: boolean;
 }
 export interface KChannelRoleUpdateResponse {
-  role_id: number;
+  role: number;
   allow: number;
   deny: number;
-}
-export interface ChannelRoleSyncResponse {
-  permissionOverwrites: permissionOverwrites
-  permissionUsers: permissionUsers,
-}
-export interface KChannelRoleSyncResponse {
-  permissionOverwrites: permissionOverwrites
-  permissionUsers: permissionUsers,
 }
 
 export type ChannelRoleUpdateResponse = KChannelRoleUpdateResponse;
